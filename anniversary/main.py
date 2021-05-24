1aec1a09ba4a07c93a68da90f11a84728e329082
Isaac Tetteh <itetteh@paretointel.com>
2021-05-24 13:06:00 -0500

    change return type and clean up code in main,py

--- anniversary/main.py
+++ anniversary/main.py
@@ -1,34 +1,38 @@
 import datetime
+import json
 import logging
+from typing import Dict
 
 import pyarrow.parquet as pq
 import s3fs
 
-client_kwargs = {"client_kwargs": {"endpoint_url": "http://minio:9000"}}
+client_kwargs = {"client_kwargs": {"endpoint_url": "http://minio:9000"}} # this is used to simulation s3
 s3 = s3fs.S3FileSystem(**client_kwargs)
 
 
-def get_employees_anniversary(event, context):
+def get_employees_anniversary(event: Dict[str, str], context: Dict[str, str]) -> str:
     """
     lambda handler to desmostrate using containers with third party packages like pandas and
     integration with s3.
 
-    Funtion gets list of employees celebrating anniversary using the number of years of services specified in request payload.
+    Funtion gets list of employees employeed for a specific number of years using the number of years of services specified in request payload.
 
     """
 
     try:
         # get required data from payload
-        year = int(event["year"])
+        year = int(event["years_of_service"])
         bucket = event["bucket"]
         key = event["key"]
         destination = event["destination"]  # full s3 path with key
-    except ValueError:
-        logging.error({"error": "Year must be a valid 4 digit year"})
-        return False
+    except ValueError as e:
+        msg = "Year must be a valid 4 digit year"
+        logging.error(msg=msg)
+        return json.dumps({"error_message": msg})
     except KeyError as e:
-        logging.error({"error": f"{e} required in payload"})
-        return False
+        msg = f"{e} required in payload"
+        logging.error(msg=msg)
+        return json.dumps({"error_message": msg})
 
     try:
         df = pq.read_table(f"s3://{bucket}/{key}", filesystem=s3).to_pandas(
@@ -40,6 +44,12 @@ def get_employees_anniversary(event, context):
 
         df.to_csv(destination, storage_options=client_kwargs)
 
-        return {"status": "done", "result_path": destination}
+        return json.dumps(
+            {
+                "status": "done",
+                "result": {"file_path": destination, "rows_returned": len(df)},
+            }
+        )
     except Exception as e:
-        return logging.error(e)
+        logging.error(e)
+        return json.dumps({"error_message": e})
